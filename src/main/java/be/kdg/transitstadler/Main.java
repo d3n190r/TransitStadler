27022c5951de13c6612ee23a633e008820d75647
igor <rogi.tr.8@gmail.com>
2026-03-15 17:19:42 +0100

    Add model to Main.java

--- src/main/java/be/kdg/transitstadler/Main.java
+++ src/main/java/be/kdg/transitstadler/Main.java
@@ -1,11 +1,16 @@
 package be.kdg.transitstadler;
 
+import be.kdg.transitstadler.model.TransitStadlerModel;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * @author Igor Goossens (INF 101)
+ */
 public class Main  extends Application {
     @Override
     public void start(Stage primaryStage) {
+        TransitStadlerModel model = new TransitStadlerModel();
         primaryStage.setTitle("TransitStadler");
         primaryStage.show();
     }
