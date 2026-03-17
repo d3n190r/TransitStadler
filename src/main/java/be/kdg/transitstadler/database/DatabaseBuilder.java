d07643798d24fed82d57b6a165fc009ff0f9a917
igor <rogi.tr.8@gmail.com>
2026-03-17 18:17:32 +0100

    Added true/false check to success of query executions in DatabaseBuilder.buildDatabase

--- src/main/java/be/kdg/transitstadler/database/DatabaseBuilder.java
+++ src/main/java/be/kdg/transitstadler/database/DatabaseBuilder.java
@@ -15,16 +15,21 @@ public class DatabaseBuilder {
     /**
      * Makes it easy to create the required tables in the database.
      * @param dropEverything If true the whole schema will be dropped before rebuilding it.
+     * @return A boolean indicating whether any query failed to execute.
      */
-    public static void buildDatabase(boolean dropEverything) {
+    public static boolean buildDatabase(boolean dropEverything) {
+        boolean success = true;
+
         if (dropEverything) {
-            Database.executeAny("DROP SCHEMA IF EXISTS public CASCADE");
-            Database.executeAny("CREATE SCHEMA public");
+            success &= Database.executeAny("DROP SCHEMA IF EXISTS public CASCADE");
+            success &= Database.executeAny("CREATE SCHEMA public");
         }
 
-        Database.executeAny("CREATE TABLE IF NOT EXISTS operator (operatorId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, operatorName VARCHAR(255) UNIQUE NOT NULL);");
-        Database.executeAny("CREATE TABLE IF NOT EXISTS station (stationId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, stationName VARCHAR(255) UNIQUE NOT NULL);");
-        Database.executeAny("CREATE TABLE IF NOT EXISTS line (lineId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, lineName VARCHAR(255) NOT NULL, operatorId INTEGER REFERENCES operator(operatorId) NOT NULL, UNIQUE(lineName, operatorId));");
-        Database.executeAny("CREATE TABLE IF NOT EXISTS stop (lineId INTEGER REFERENCES line(lineId), stationId INTEGER REFERENCES station(stationId), stopSequence int NOT NULL, PRIMARY KEY(lineId, stationId), FOREIGN KEY (stationId) REFERENCES station(stationId), FOREIGN KEY (lineId) REFERENCES line(lineId), UNIQUE (lineId, stopSequence));");
+        success &= Database.executeAny("CREATE TABLE IF NOT EXISTS operator (operatorId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, operatorName VARCHAR(255) UNIQUE NOT NULL);");
+        success &= Database.executeAny("CREATE TABLE IF NOT EXISTS station (stationId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, stationName VARCHAR(255) UNIQUE NOT NULL);");
+        success &= Database.executeAny("CREATE TABLE IF NOT EXISTS line (lineId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, lineName VARCHAR(255) NOT NULL, operatorId INTEGER REFERENCES operator(operatorId) NOT NULL, UNIQUE(lineName, operatorId));");
+        success &= Database.executeAny("CREATE TABLE IF NOT EXISTS stop (lineId INTEGER REFERENCES line(lineId), stationId INTEGER REFERENCES station(stationId), stopSequence int NOT NULL, PRIMARY KEY(lineId, stationId), FOREIGN KEY (stationId) REFERENCES station(stationId), FOREIGN KEY (lineId) REFERENCES line(lineId), UNIQUE (lineId, stopSequence));");
+
+        return success;
     }
 }
