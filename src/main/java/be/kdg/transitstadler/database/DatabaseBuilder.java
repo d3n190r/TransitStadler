8fa784f5942251796f42f0838cf1cc2f30de9cf4
igor <rogi.tr.8@gmail.com>
2026-03-17 18:11:16 +0100

    Made dropping the schema before building the database optional

--- src/main/java/be/kdg/transitstadler/database/DatabaseBuilder.java
+++ src/main/java/be/kdg/transitstadler/database/DatabaseBuilder.java
@@ -13,15 +13,18 @@ public class DatabaseBuilder {
     }
 
     /**
-     * Makes it easy to create the tables in the DB.
+     * Makes it easy to create the required tables in the database.
+     * @param dropEverything If true the whole schema will be dropped before rebuilding it.
      */
-    public static void buildDatabase() {
-        Database.executeAny("DROP SCHEMA IF EXISTS public CASCADE", new Object[0]);
-        Database.executeAny("CREATE SCHEMA public", new Object[0]);
+    public static void buildDatabase(boolean dropEverything) {
+        if (dropEverything) {
+            Database.executeAny("DROP SCHEMA IF EXISTS public CASCADE");
+            Database.executeAny("CREATE SCHEMA public");
+        }
 
-        Database.executeAny("CREATE TABLE IF NOT EXISTS operator (operatorId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, operatorName VARCHAR(255) UNIQUE NOT NULL);", new Object[0]);
-        Database.executeAny("CREATE TABLE IF NOT EXISTS station (stationId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, stationName VARCHAR(255) UNIQUE NOT NULL);", new Object[0]);
-        Database.executeAny("CREATE TABLE IF NOT EXISTS line (lineId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, lineName VARCHAR(255) NOT NULL, operatorId INTEGER REFERENCES operator(operatorId) NOT NULL, UNIQUE(lineName, operatorId));", new Object[0]);
-        Database.executeAny("CREATE TABLE IF NOT EXISTS stop (lineId INTEGER REFERENCES line(lineId), stationId INTEGER REFERENCES station(stationId), stopSequence int NOT NULL, PRIMARY KEY(lineId, stationId), FOREIGN KEY (stationId) REFERENCES station(stationId), FOREIGN KEY (lineId) REFERENCES line(lineId), UNIQUE (lineId, stopSequence));", new Object[0]);
+        Database.executeAny("CREATE TABLE IF NOT EXISTS operator (operatorId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, operatorName VARCHAR(255) UNIQUE NOT NULL);");
+        Database.executeAny("CREATE TABLE IF NOT EXISTS station (stationId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, stationName VARCHAR(255) UNIQUE NOT NULL);");
+        Database.executeAny("CREATE TABLE IF NOT EXISTS line (lineId INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, lineName VARCHAR(255) NOT NULL, operatorId INTEGER REFERENCES operator(operatorId) NOT NULL, UNIQUE(lineName, operatorId));");
+        Database.executeAny("CREATE TABLE IF NOT EXISTS stop (lineId INTEGER REFERENCES line(lineId), stationId INTEGER REFERENCES station(stationId), stopSequence int NOT NULL, PRIMARY KEY(lineId, stationId), FOREIGN KEY (stationId) REFERENCES station(stationId), FOREIGN KEY (lineId) REFERENCES line(lineId), UNIQUE (lineId, stopSequence));");
     }
 }
