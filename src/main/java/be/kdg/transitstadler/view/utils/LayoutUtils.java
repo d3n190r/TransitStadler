2315d10adde1894375f2a4b78df9a10ef51bbdfd
igor <igor.goossens@student.kdg.be>
2026-03-18 12:50:48 +0100

    Added documentation and changed parameter names in LayoutUtils

--- src/main/java/be/kdg/transitstadler/view/utils/LayoutUtils.java
+++ src/main/java/be/kdg/transitstadler/view/utils/LayoutUtils.java
@@ -6,22 +6,47 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+/**
+ * @author Igor Goossens (INF 101)
+ */
 public class LayoutUtils {
-    public static void applyMarginsToChildren(HBox container, double margin) {
-        for (Node child : container.getChildren()) {
-            HBox.setMargin(child, new javafx.geometry.Insets(margin));
+    /* applyMarginsToChildren gives all !ALREADY ADDED! children of the container the same margin.
+     * Already made for the following containers:
+     * - HBox
+     * - VBox
+     * - BorderPane
+     */
+
+    /**
+     * Sets a given margin to all the elements currently in the container.
+     * @param hBox The hbox whose elements should have the margin.
+     * @param margin The amount of margin to set.
+     */
+    public static void applyMarginsToChildren(HBox hBox, double margin) {
+        for (Node currentChild : hBox.getChildren()) {
+            HBox.setMargin(hBox, new javafx.geometry.Insets(margin));
         }
     }
 
-    public static void applyMarginsToChildren(VBox container, double margin) {
-        for (Node child : container.getChildren()) {
-            VBox.setMargin(child, new javafx.geometry.Insets(margin));
+    /**
+     * Sets a given margin to all the elements currently in the container.
+     * @param vBox The vbox whose elements should have the margin.
+     * @param margin The amount of margin to set.
+     */
+    public static void applyMarginsToChildren(VBox vBox, double margin) {
+        for (Node currentChild : vBox.getChildren()) {
+            VBox.setMargin(currentChild, new javafx.geometry.Insets(margin));
         }
     }
 
-    public static void applyMarginsToChildren(BorderPane container, double margin) {
-        for (Node child : container.getChildren()) {
-            BorderPane.setMargin(child, new Insets(margin));
+    /**
+     * Sets a given margin to all the elements currently in the container.
+     * @param borderPane The borderpane whose elements should have the margin.
+     * @param margin The amount of margin to set.
+     */
+    public static void applyMarginsToChildren(BorderPane borderPane, double margin) {
+        for (Node currentChild : borderPane.getChildren()) {
+            BorderPane.setMargin(currentChild, new Insets(margin));
         }
     }
 }
